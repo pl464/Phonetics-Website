d4e5e61e0868dde71bf6d7371236d2ab470e5eb4
Patrick Lee <pl464@scarletmail.rutgers.edu>
2020-08-09 22:29:43 -0400

    Button toggle for lesson/practice options

--- home.js
+++ home.js
@@ -2,27 +2,27 @@ const BUTTON_SIZE = 100;
 
 class App {
     constructor (){
-        this.$button = document.querySelector('.button');
-        
+        this.$button = document.querySelectorAll('.button');
         this.addEventListeners();
     }
     
     addEventListeners() {
-        this.$button.addEventListener('click', event => {
-            const bound = event.target.getBoundingClientRect();
-            const hCenter = (bound.left + bound.right) / 2;
-            const vCenter = (bound.top + bound.bottom) / 2;
-            const clickDistance = Math.sqrt(Math.pow(event.clientX - hCenter, 2) + Math.pow(event.clientY - vCenter, 2));
-            if (clickDistance > BUTTON_SIZE/2) return;
-            console.log("Button clicked");
-            
-            
+        this.$button.forEach( (button) => {
+            button.addEventListener('click', event => {
+                const bound = event.target.getBoundingClientRect();
+                const hCenter = (bound.left + bound.right) / 2;
+                const vCenter = (bound.top + bound.bottom) / 2;
+                const clickDistance = Math.sqrt(Math.pow(event.clientX - hCenter, 2) + Math.pow(event.clientY - vCenter, 2));
+                if (clickDistance > BUTTON_SIZE/2) return;
+                console.log("Button clicked");
+                console.log(button.id);
+                const option = document.querySelector(`#${button.id}+.options`);
+                option.style.display = 'flex';
+            });    
         });
-        
         
     }
                                                    
 }
-
-console.log("hi");                                                   
+                                        
 new App();
